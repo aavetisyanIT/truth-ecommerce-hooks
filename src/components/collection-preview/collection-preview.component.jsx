2fd43fef7f735a7f56ea088fb0a6df30b8acc932
aavetisyanit <aavetisyanit@gmail.com>
2021-03-02 18:51:11 -0600

    Add collection context to collection-preview component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
+import CollectionsContext from './../../contexts/collections/collections.context';
 
-const CollectionPreview = ({ title, items }) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
-    <div className='preview'>
-      {items
-        .filter((item, idx) => idx < 4)
-        .map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-    </div>
-  </div>
-);
+const CollectionPreview = ({ title }) => {
+	const collections = useContext(CollectionsContext);
+	return (
+		<div className='collection-preview'>
+			<h1 className='title'>{title.toUpperCase()}</h1>
+			<div className='preview'>
+				{collections[title].items
+					.filter((item, idx) => idx < 4)
+					.map(item => (
+						<CollectionItem key={item.id} item={item} />
+					))}
+			</div>
+		</div>
+	);
+};
 
 export default CollectionPreview;
